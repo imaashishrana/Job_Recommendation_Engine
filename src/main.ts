973bf3e7ddd70bc2bf0a2e7a887ad97e81eb9f4a
Ashish Rana <rajputaashu1719@gmail.com>
2026-09-12 11:41:26 +0530

    Add environment configuration and app entrypoint main.ts

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`Job Match API is running on: http://localhost:${port}`);
+}
+bootstrap();
